a2cb2fc64ec7dfe0957032116bd411c78179f2e1
Liam Heeger <lheeger@u.rochester.edu>
2018-09-01 15:41:24 -0400

    Adding signal manager class.

---
+++ shell/signal_manager.cc
@@ -0,0 +1,6 @@
+//
+// Created by Liam Heeger on 9/1/18.
+//
+
+#include "signal_manager.h"
+
---
+++ shell/signal_manager.h
@@ -0,0 +1,15 @@
+//
+// Created by Liam Heeger on 9/1/18.
+//
+
+#ifndef SHELL_SIGNAL_MANAGER_H
+#define SHELL_SIGNAL_MANAGER_H
+
+namespace shell {
+    class SignalManager {
+
+    };
+}
+
+
+#endif //SHELL_SIGNAL_MANAGER_H
